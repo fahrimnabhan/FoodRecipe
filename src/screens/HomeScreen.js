302f701981045b2ee8c40bc64433d7e21783150a
fahrimnabhan <fm.nabhan@rocketmail.com>
2024-11-25 02:11:41 -0500

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -729,12 +729,16 @@ export default function HomeScreen() {
         </View>
 
         <View testID="categoryList">
-       
+            <Categories
+                categories={categories}
+                activeCategory={activeCategory}
+                handleChangeCategory={handleChangeCategory}
+            />
         </View>
 
         <View testID="foodList">
-
-          </View>
+            <Recipe foods={filteredfoods} categories={categories} />
+        </View>
       </ScrollView>
     </View>
   );
